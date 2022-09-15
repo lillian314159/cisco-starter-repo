33d7f4f3c94bca085624e8cc9b7c242be542dae9
Lillian <lillian314159@gmail.com>
2022-09-15 12:50:47 -0700

    Added card-style componentsfor IP and Pylon latency

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Banner(props) {
+  return (
+    <h1>Sextant</h1>
+  );
+}
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="Card">
+        <h2>{this.props.title}</h2>
+        <p>{this.props.data}</p>
+      </div>
+    );
+  }
+}
+
+class Exhibit extends React.Component {
+  render() {
+    return (
+      <div className="Exhibit">
+        <Card title="Public IP" data="TODO" />
+        <Card title="Latency Information for Pylon" data="TODO" />
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <Banner />
+        </header>
+        <body>
+          <Exhibit />
+        </body>
     </div>
   );
 }
